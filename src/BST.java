a9e780141bde4cfc312006c4f67e7179c40883f7
student-repo <repostudnet@gmail.com>
2017-09-03 09:18:37 +0200

    Ready project

--- src/BST.java
+++ src/BST.java
@@ -1,22 +1,6 @@
 import java.util.Stack;
 
-/**
- * BST.java Class for a binary search tree.
- *
- * Has a public inner class Node for individual nodes. The inner class is public
- * because some methods take references to nodes as parameter or return
- * references to nodes. Unlike the BinaryTree class, the BST class needs to
- * maintain an instance variable for the root, because structural changes to a
- * BST can mean that different nodes are the root at different times.
- *
- * @author Tom Cormen
- */
-
 public class BST {
-    // The public inner class for individual nodes.
-    // The instance variables left, right, and parent are protected because you
-    // might want to make subclasses for balanced binary search trees, and they
-    // need access to the instance variables of Node objects.
 
     public static void main(String[] args){
         BST BST = new BST();
@@ -73,54 +57,33 @@ public class BST {
         protected Node parent;      // this Node's parent
         private int size;
         private int key;              // this Node's key
-        /**
-         * Constructor for a Node.
-         *
-         * @param key this node's key
-         */
+
         public Node(int key) {
             this.key = key;
             this.size = 1;
             parent = left = right = null;
         }
 
-        /**
-         * @return the key of this Node
-         */
         public int getKey() {
             return key;
         }
-        /**
-         * @return the String representation of this Node
-         */
+
         public String toString() {
             return "key = " + key;
         }
     }
 
 
-    // Instance variables for the BST<K,V> class. They are protected so that
-    // subclasses can access them.
-    protected Node root;      // root of this BST
+    protected Node root;
 
-
-    /**
-     * Constructor for a BST. Makes an empty BST.
-     */
     public BST() {
         root = null;
     }
 
-    /**
-     * Return a boolean indicating whether this BST is empty.
-     */
     public boolean isEmpty() {
         return root == null;
     }
 
-    /**
-     * Return a reference to the root, or null if this BST is empty.
-     */
     public Node getRoot() {
         if (root == null)
             return null;
@@ -175,11 +138,7 @@ public class BST {
         }
     }
 
-    /**
-     * Return a String representation of this BST, indenting each level by two
-     * spaces. Right subtrees appear before subtree roots, which appear before
-     * left subtrees, so that when viewed sideways, we see the BST structure.
-     */
+
     public String toString() {
         if (root == null)
             return "";
@@ -187,9 +146,6 @@ public class BST {
             return print(root, 0);
     }
 
-    /**
-     * Return a string of 2*s spaces, for indenting.
-     */
     private String indent(int s) {
         String result = "";
         for (int i = 0; i < s; i++)
@@ -197,13 +153,7 @@ public class BST {
         return result;
     }
 
-    /**
-     * Return a String representing the subtree rooted at a node.
-     *
-     * @param x the root of the subtree
-     * @param depth the depth of x in the BST
-     * @return the String representation of the subtree rooted at x
-     */
+
     private String print(Node x, int depth) {
         if (x == null)
             return "";
@@ -212,21 +162,12 @@ public class BST {
                     + print(x.left, depth + 1);
     }
 
-    /**
-     * Create a new node and insert it into the BST.
-     *
-     * @param key key of the new node
-     * @return a reference to the new node
-     */
     public Node insert(int key) {
-        Node z = new Node(key);  // create the new Node
-        Node x = root;                  // Node whose key is compared with z's
+        Node z = new Node(key);
+        Node x = root;
         Node y;
-        Node xParent = null;        // x's parent
+        Node xParent = null;
 
-        // Go down the BST from the root, heading left or right depending on
-        // how the new key compares with x's key, until we find a missing node,
-        // indicated by the null.
         while (x != null) {
             xParent = x;
             if (key < x.key)
@@ -235,13 +176,11 @@ public class BST {
                 x = x.right;
         }
 
-        // At this point, we got down to the null. Make the last non-null
-        // node be x's parent and x the appropriate child.
         z.parent = xParent;
 
-        if (xParent == null)  // empty BST?
-            root = z;               // then just the one node
-        else {                    // link z as the appropriate child of x's parent
+        if (xParent == null)
+            root = z;
+        else {
             if (key < xParent.key)
                 xParent.left = z;
             else
@@ -272,87 +211,60 @@ public class BST {
         return maximum(root);
     }
 
-    /**
-     * Replace the subtree rooted at node u with the subtree rooted at node v.
-     * Note: This method does not change v.left or v.right; it is the caller's
-     * responsibility to do so.
-     *
-     * @param u the node to be replaced
-     * @param v the replacement node
-     */
     protected void transplant(Node u, Node v) {
-        if (u.parent == null)     // was u the root?
-            root = v;                   // if so, now v is the root
-        else if (u == u.parent.left)  // otherwise adjust the child of u's parent
+        if (u.parent == null)
+            root = v;
+        else if (u == u.parent.left)
             u.parent.left = v;
         else
             u.parent.right = v;
 
-        if (v != null)      // if v wasn't the null ...
-            v.parent = u.parent;  // ... update its parent
+        if (v != null)
+            v.parent = u.parent;
     }
 
-    /**
-     * Remove node z from a BST. Note: Unlike many BST remove procedures, this one
-     * is guaranteed to remove node z, and not some other node.
-     *
-     * @param z the node to remove
-     */
     public void remove(Node z) {
         Node yy  = z;
         while(yy.parent != null) {
             yy = yy.parent;
             yy.size --;
         }
-        if (z.left == null)       // no left child?
-            transplant(z, z.right);     // then just replace z by its right child
-        else if (z.right == null) // no right child?
-            transplant(z, z.left);      // then just replace z by its left child
+        if (z.left == null)
+            transplant(z, z.right);
+        else if (z.right == null)
+            transplant(z, z.left);
         else {
-            // Node z has two children.
-            Node y = minimum(z.right);  // y is in z's right subtree, and y has no left
-            // child
+            Node y = minimum(z.right);
 
-            // Splice y out of its current location, and have it replace z in the BST.
+
             if (y.parent != z) {
-                // If y is not z's right child, replace y as a child of its parent by
-                // y's right child and turn z's right child into y's right child.
+
                 transplant(y, y.right);
                 y.right = z.right;
                 y.right.parent = y;
             }
 
-            // Regardless of whether we found that y was z's right child, replace z as
-            // a child of its parent by y and replace y's left child by z's left
-            // child.
             transplant(z, y);
             y.left = z.left;
             y.left.parent = y;
         }
     }
 
-    /**
-     * Return a reference to the successor of node x, or null if x has no successor.
-     */
+
     public Node successor(Node x) {
         if (x.right != null)
-            // If x has a right subtree, the successor is the node in the right
-            // subtree with the minimum key.
+
+
             return minimum(x.right);
         else {
-            // Otherwise, the successor is the lowest ancestor of x whose left child
-            // is also an ancestor of x.
             Node y = x.parent;
 
-            // Go up from x's parent toward the root until finding a left child or the
-            // root. x's successor is the parent of that first left child.
             while (y != null && x == y.right) {
                 x = y;
                 y = y.parent;
             }
 
-            // Node y is the parent of the first left child on the path from x's
-            // parent to the root, or the root if we hit it.  Return node y.
+
             if (y == null)
                 return null;
             else
@@ -360,28 +272,18 @@ public class BST {
         }
     }
 
-    /**
-     * Return a reference to the predecessor of node x, or null if x has no predecessor.
-     */
     public Node predecessor(Node x) {
-        // If x has a left subtree, the predecessor is the node in the left
-        // subtree with the maximum key.
         if (x.left != null)
             return maximum(x.left);
         else {
-            // Otherwise, the predecessor is the lowest ancestor of x whose right child
-            // is also an ancestor of x.
+
             Node y = x.parent;
 
-            // Go up from x's parent toward the root until finding a right child or the
-            // root. x's successor is the parent of that first right child.
+
             while (y != null && x == y.left) {
                 x = y;
                 y = y.parent;
             }
-
-            // Node y is the parent of the first right child on the path from x's
-            // parent to the root, or the root if we hit it.  Return node y.
             if (y == null)
                 return null;
             else
@@ -389,23 +291,15 @@ public class BST {
         }
     }
 
-    /**
-     * Return a reference to the node in the subtree rooted at x with the minimum
-     * key.
-     */
     public Node minimum(Node x) {
-        // Keep going to the left until finding a node with no left child. That node
-        // is the minimum node in x's subtree.
+
         while (x.left != null)
             x = x.left;
 
         return x;
     }
 
-    /**
-     * Return a reference to the node in the subtree rooted at x with the maximum
-     * key.
-     */
+
     public Node maximum(Node x) {
         // Keep going to the left until finding a node with no right child. That node
         // is the maximum node in x's subtree.
@@ -415,17 +309,9 @@ public class BST {
         return x;
     }
 
-    /**
-     * Search for a node in the subtree rooted at x with a specific key.
-     * @param key the key we're searching for
-     * @return a reference to the node whose key equals the parameter, or null if
-     * no such node in x's subtree
-     */
     public Node search(int key) {
         Node x = root;
 
-        // Go down the left or right subtree until either we hit the null or
-        // find the key.
         while (x != null && key != x.key ) {
             if (key < x.key)
                 x = x.left;
@@ -433,7 +319,6 @@ public class BST {
                 x = x.right;
         }
 
-        // If we got to the null, the key was not in the BST.
         if (x == null)
             return null;
         else
--- src/Task2.java
+++ src/Task2.java
@@ -10,36 +10,28 @@ public class Task2 {
         int sameDataSizeTestNumber = 10;
         int testJump = 10;
         int testNumber = 100;
-//        RandomString rs = new RandomString(randomStringLength, 'a', 'z');
-        String report = "String length, lsc, recursion lsc\n";
+        String report = "String length, lsc\n";
         Long lscTime = Long.valueOf(0);
-        Long recursionLscTime = Long.valueOf(0);
 
 //        System.out.println("hello world");
-//        System.out.println(lcs("bdcaba", "abcbdab"));
+        System.out.println(lcs("bdcaba", "abcbdab"));
 //        System.out.println(longestCommonSubsequence(rs.nextString(), rs.nextString()));
-//        System.out.println(getLongestCommonSubsequence("bdcaba", "abcbdab"));
+//        System.out.println(lcs("bdcaba", "abcbdab"));
 
         for(int i = testJump; i < testJump * testNumber; i += testJump) {
             lscTime = Long.valueOf(0);
-            recursionLscTime = Long.valueOf(0);
             for(int j = 0; j < sameDataSizeTestNumber; j++) {
                 long startTime;
                 long stopTime;
                 RandomString rs = new RandomString(i, 'a', 'z');
 
                 startTime = System.nanoTime();
-                longestCommonSubsequence(rs.nextString(), rs.nextString());
+                lcs(rs.nextString(), rs.nextString());
                 stopTime = System.nanoTime();
                 lscTime += (stopTime - startTime);
 
-                startTime = System.nanoTime();
-                getLongestCommonSubsequence(rs.nextString(), rs.nextString());
-                stopTime = System.nanoTime();
-                recursionLscTime += (stopTime - startTime);
-
             }
-            report += (i + "," + String.format("%.3f", (double) lscTime / (double) sameDataSizeTestNumber)) + ", "  + String.format("%.3f", ((double) recursionLscTime / (double) sameDataSizeTestNumber)) + "\n";
+            report += (i + "," + String.format("%.3f", ((double) lscTime / (double) sameDataSizeTestNumber)) + "\n");
         }
 
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
@@ -56,81 +48,11 @@ public class Task2 {
         Runtime runTime = Runtime.getRuntime();
         Process process = runTime.exec("libreoffice --calc ./src/task2_output.csv &");
 
-
-
-
-
-
     }
 
-    public static int lcs(String a, String b, int i, int j) {
-        int x;
-        if(a.charAt(i) == b.charAt(j)) {
-            x = lcs(a, b, i - 1, j - 1) + 1;
-        } else {
-            x = Math.max(lcs(a, b, i -1, j), lcs(a, b, i, j - 1));
-        }
-        return x;
-    }
+    
 
-    public static int longestCommonSubsequence(String A, String B) {
-        if (A == null || B == null || A.length() == 0 || B.length() == 0) {
-            return 0;
-        }
-        int[][] check = new int[A.length()  + 1][B.length() + 1];
-        for (int i = 1; i <= A.length(); i++) {
-            for (int j = 1; j <= B.length(); j++) {
-                if (A.charAt(i - 1) == B.charAt(j - 1)) {
-                    check[i][j] = check[i - 1][j - 1] + 1;
-                } else {
-                    check[i][j] = Math.max(check[i][j], check[i - 1][j]);
-                    check[i][j] = Math.max(check[i][j], check[i][j - 1]);
-                }
-            }
-        }
-        return check[A.length()][B.length()];
-    }
-
-
-//    public static String lcs(String a, String b){
-//        int aLen = a.length();
-//        int bLen = b.length();
-//        if(aLen == 0 || bLen == 0){
-//            return "";
-//        }else if(a.charAt(aLen-1) == b.charAt(bLen-1)){
-//            return lcs(a.substring(0,aLen-1),b.substring(0,bLen-1))
-//                    + a.charAt(aLen-1);
-//        }else{
-//            String x = lcs(a, b.substring(0,bLen-1));
-//            String y = lcs(a.substring(0,aLen-1), b);
-//            return (x.length() > y.length()) ? x : y;
-//        }
-//    }
-//
-//
-//    public static int LCS(String A, String B) {
-//        if (A.length() == 0 || B.length() == 0) {
-//            return 0;
-//        }
-//        int lenA = A.length();
-//        int lenB = B.length();
-//        // check if last characters are same
-//        if (A.charAt(lenA - 1) == B.charAt(lenB - 1)) {
-//            // Add 1 to the result and remove the last character from both
-//            // the strings and make recursive call to the modified strings.
-//            return 1 + LCS(A.substring(0, lenA - 1), B.substring(0, lenB - 1));
-//        } else {
-//            // Remove the last character of String 1 and make a recursive
-//            // call and remove the last character from String 2 and make a
-//            // recursive and then return the max from returns of both recursive
-//            // calls
-//            return Math.max(
-//                    LCS(A.substring(0, lenA - 1), B.substring(0, lenB)),
-//                    LCS(A.substring(0, lenA), B.substring(0, lenB - 1)));
-//        }
-//    }
-
-    public static int getLongestCommonSubsequence(String a, String b){
+    public static int lcs(String a, String b){
         int m = a.length();
         int n = b.length();
         int[][] dp = new int[m+1][n+1];
